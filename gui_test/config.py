14cbdc0efb8f1e1c9617cdde81f2fcb62006ab65
Torje Amundsen <torjefrafen@hotmail.no>
2023-06-25 00:52:01 +0200

    90% done refactoring

--- gui_test/config.py
+++ gui_test/config.py
@@ -14,27 +14,32 @@ class Config():
         if not os.path.exists(f"{directory}/config"):
             os.makedirs(f"{directory}/config")
         
-    def keysequence_to_string(self, sequence):
+    """ def keysequence_to_string(self, sequence):
         key_sequence = sequence.keySequence()
         return key_sequence.toString()
     
     def string_to_keysequence(self, sequence_string):
         key_sequence = QKeySequence.fromString(sequence_string)
-        return key_sequence
+        return key_sequence """
     
     def save(self, ui):
         ui.dpiSelector.setCurrentText(ui.dpiSelector.currentText() or "800")
-
+        if ui.randomizeBindButton.text() in ["Set a bind...", "Invalid key!", "..."]:
+            ui.randomizeBindButton.setText("Alt+X")
+        if ui.enableBindButton.text() in ["Set a bind...", "Invalid key!", "..."]:
+            ui.enableBindButton.setText("F6")
+        if ui.disableBindButton.text() in ["Set a bind...", "Invalid key!", "..."]:
+            ui.disableBindButton.setText("F7")
         configuration = {
             "directory": ui.gameDirectoryField.text(),
             "dpi": ui.dpiSelector.currentText(),
             "min_sensitivity": ui.minSensSpinbox.value(),
             "max_sensitivity": ui.maxSensSpinbox.value(),
             "base_sensitivity": ui.defaultSensSpinbox.value(),
-            "randomize_bind": self.keysequence_to_string(ui.randomizeBindField),
+            "randomize_bind": ui.randomizeBindButton.text(),
             "timer": [ui.timerCheckbox.isChecked(), ui.timeSpinbox.value()],
-            "enable_bind": self.keysequence_to_string(ui.enableBindField),
-            "disable_bind": self.keysequence_to_string(ui.disableBindField),
+            "enable_bind": ui.enableBindButton.text(),
+            "disable_bind": ui.disableBindButton.text(),
             "theme": self.theme
         }
 
@@ -51,11 +56,11 @@ class Config():
                 ui.minSensSpinbox.setValue(configuration.get("min_sensitivity", 0.7))
                 ui.maxSensSpinbox.setValue(configuration.get("max_sensitivity", 3.8))
                 ui.defaultSensSpinbox.setValue(configuration.get("base_sensitivity", 1.5))
-                ui.randomizeBindField.setKeySequence(self.string_to_keysequence(configuration.get("randomize_bind", "Alt+X")))
+                ui.randomizeBindButton.setText(configuration.get("randomize_bind", "Set a bind..."))
                 ui.timerCheckbox.setChecked(configuration.get("timer")[0])
                 ui.timeSpinbox.setValue(configuration.get("timer")[1])
-                ui.enableBindField.setKeySequence(self.string_to_keysequence(configuration.get("enable_bind", "F6")))
-                ui.disableBindField.setKeySequence(self.string_to_keysequence(configuration.get("disable_bind", "F7")))
+                ui.enableBindButton.setText(configuration.get("enable_bind", "Set a bind..."))
+                ui.disableBindButton.setText(configuration.get("disable_bind", "Set a bind..."))
                 self.theme = (configuration.get("theme", 0))
             
             print("Configuration loaded successfully")
