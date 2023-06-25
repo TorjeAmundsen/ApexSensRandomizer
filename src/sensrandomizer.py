d04ded210fc505385fabfe23608cd908d8fc8aa0
Torje Amundsen <torjefrafen@hotmail.no>
2023-06-25 15:08:16 +0200

    Optimizations for v1.1.0

--- src/sensrandomizer.py
+++ src/sensrandomizer.py
@@ -1,34 +1,32 @@
 # Made by Torje Amundsen
 import sys
-import random
-import os
+from random import uniform as uniform_random
+from os import path, remove
 from PyQt6 import QtWidgets, QtGui
 from PyQt6.QtWidgets import QApplication, QFileDialog, QMessageBox
 from gui import Ui_MainWindow as QtGUI
-import datetime
-import base64
+from datetime import datetime
+from base64 import b64decode
 from config import Config
-import vdf
-import winreg
-import threading
-import time
-import keyboard
-import requests
-import webbrowser
+from vdf import load as vdfload
+from winreg import OpenKey, QueryValueEx, HKEY_LOCAL_MACHINE
+from threading import Event, Thread
+from time import sleep
+from keyboard import add_hotkey, read_key, remove_all_hotkeys
+from requests import get as request
+from webbrowser import open_new_tab
 
 def main():
     def check_for_updates(release, ui, window):
         owner = "TorjeAmundsen"
         repo = "ApexSensRandomizer"
-        response = requests.get(f"https://api.github.com/repos/{owner}/{repo}/releases/latest")
+        response = request(f"https://api.github.com/repos/{owner}/{repo}/releases/latest")
         response_json = response.json()
         tag = response_json.get("tag_name", "")
         assets = response_json.get("assets")
         download = assets[0]["browser_download_url"]
         if response.status_code == 200:
             if tag not in [release, config.skipped_update_tag]:
-                # [f"Download {release_tag}", "Show Changelog", "Remind Me Later", "Don't Ask Again"]
-                print("placeholder for showing a new update is available")
                 update = QMessageBox(window)
                 update.setWindowTitle("Update")
                 update.setText("A new update is available for ApexSensRandomizer!")
@@ -36,15 +34,15 @@ def main():
                 update.setPalette(ui.palette)
                 update_button = update.addButton("Download", QMessageBox.ButtonRole.YesRole)
                 update_button.clicked.connect(lambda: open_site(download))
-                changelog_button = update.addButton("Show Changelog", QMessageBox.ButtonRole.YesRole)
+                changelog_button = update.addButton("Changelog", QMessageBox.ButtonRole.YesRole)
                 changelog_button.clicked.disconnect()
                 changelog_button.clicked.connect(lambda: open_site(f"https://www.github.com/{owner}/{repo}/releases/latest"))
                 skip_update_button = update.addButton("Skip Version", QMessageBox.ButtonRole.YesRole)
                 skip_update_button.clicked.connect(lambda: skip_update(tag))
-                update.addButton("Update Later", QMessageBox.ButtonRole.NoRole)
+                update.addButton("Not Now", QMessageBox.ButtonRole.NoRole)
                 update.exec()
     def open_site(url):
-        webbrowser.open_new_tab(url)
+        open_new_tab(url)
     
     def skip_update(tag):
         config.skipped_update_tag = tag
@@ -56,22 +54,22 @@ def main():
         for i in disabled_while_running:
             if i != ui.outputLabel and i != ui.startRandomizerButton:
                 i.setEnabled(not ui.running)
-        now = datetime.datetime.now()
+        now = datetime.now()
         sensLog = open(sens_log_txt, "a")
         sensLog.write(f"\n[{now.strftime('%Y-%m-%d %H:%M:%S] ')}{start_stop_log[ui.running]}")
     def start_randomizer():
         generate_autoexec()
-        if os.path.isfile(ui.gameDirectoryField.text() + "/cfg/enablerando.cfg"):
+        if path.isfile(ui.gameDirectoryField.text() + "/cfg/enablerando.cfg"):
             toggle()
             if ui.running:
-                    keyboard.add_hotkey(config.update_bind_modifiers(ui.randomizeBindButton.text(), ui), randomize)
+                    add_hotkey(config.update_bind_modifiers(ui.randomizeBindButton.text(), ui), randomize)
                     ui.outputLabel.setText(f"Press {config.update_bind_modifiers(ui.randomizeBindButton.text(), ui)}")
                     if ui.timerCheckbox.isChecked():
                         startThreadedFunction(timerLoop, ui.timeSpinbox.value())
             else:
                 ui.outputLabel.setText(f"Not running")
                 reset_sensitivity()
-                keyboard.remove_all_hotkeys()
+                remove_all_hotkeys()
                 event.clear()
 
     def browse_directory():
@@ -85,12 +83,12 @@ def main():
             ui.gameDirectoryField.setText(f"{browse}/")
 
     def findSteamDirectory():
-        key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, R"SOFTWARE\WOW6432Node\Valve\Steam")
-        path = winreg.QueryValueEx(key, "InstallPath")
+        key = OpenKey(HKEY_LOCAL_MACHINE, R"SOFTWARE\WOW6432Node\Valve\Steam")
+        path = QueryValueEx(key, "InstallPath")
         return path[0]
 
     def apexLibraryPath(id):
-        folders = vdf.load(open(rf"{findSteamDirectory()}\steamapps\libraryfolders.vdf"))
+        folders = vdfload(open(rf"{findSteamDirectory()}\steamapps\libraryfolders.vdf"))
         vdfstring = folders.get("libraryfolders")
 
         for i in vdfstring.values():
@@ -101,7 +99,7 @@ def main():
 
     def auto_detect_directory():
         appmanifestpath = rf"{apexLibraryPath(apexID)}\steamapps\appmanifest_{apexID}.acf"
-        appmanifest = vdf.load(open(appmanifestpath))
+        appmanifest = vdfload(open(appmanifestpath))
         for i in appmanifest.values():
             print("Path detected: ", rf"{apexLibraryPath(apexID)}\steamapps\common\{i.get('installdir')}")
             ui.gameDirectoryField.setText(rf"{apexLibraryPath(apexID)}\steamapps\common\{i.get('installdir')}")
@@ -112,7 +110,7 @@ def main():
                 if i != args:
                     i.setEnabled(False)
             args.setText("Press a key...")
-        newThread = threading.Thread(target=function, args=(args,))
+        newThread = Thread(target=function, args=(args,))
         newThread.daemon = True
         newThread.start()
 
@@ -125,10 +123,10 @@ def main():
             if timer == delay*10:
                 randomize()
                 timer = 0
-            time.sleep(0.1)
+            sleep(0.1)
 
     def recordKey(bind):
-        k = keyboard.read_key()
+        k = read_key()
         if (k.isalnum() and not len(k) > 1 or k.startswith("f")):
                 k = str(k).upper()
                 bind.setText(k)
@@ -140,7 +138,7 @@ def main():
     def randomize():
         min_float = float(ui.minSensSpinbox.value())
         max_float = float(ui.maxSensSpinbox.value())
-        init_RNG = random.uniform(min_float, max_float)
+        init_RNG = uniform_random(min_float, max_float)
         sens_num_actual = round(init_RNG, 2)
         floatSens = float(sens_num_actual)
 
@@ -148,7 +146,7 @@ def main():
 
         formattedSens = cmRev + "cm/360 (" + f"{floatSens:.2f}" + " @ " + str(ui.dpiSelector.currentText()) + " DPI)"
 
-        now = datetime.datetime.now()
+        now = datetime.now()
         sensLog = open(sens_log_txt, "a")
         sensLog.write("\n[" + now.strftime("%Y-%m-%d %H:%M:%S] ") + formattedSens)
 
@@ -222,7 +220,7 @@ mouse_sensitivity {ui.defaultSensSpinbox.value()}"""
 
     
     FROZEN = hasattr(sys, "frozen")
-    sens_randomizer_directory = os.path.dirname(sys.executable if FROZEN else os.path.abspath(__file__))
+    sens_randomizer_directory = path.dirname(sys.executable if FROZEN else path.abspath(__file__))
 
     live_sens_txt = f"{sens_randomizer_directory}/config/current_sensitivity.txt"
     sens_log_txt = f"{sens_randomizer_directory}/config/sensitivity_log.txt"
@@ -261,7 +259,7 @@ AADgAAAAAAAAD6AAAAiAAAAK8AAAAAAAAAD5AAAIgAAAnwAAAAAAAAAAD7IACIAAK/AAAAAAAAAA
 AAD/pBmRSv8AAAAAAAAAAAAAAP////8AAAAAAAD/wAP//4AB//4AAH/+AAB//AAAP/wAAD/8AAA/
 +AAAH/gAAB/4AAAf+AAAH/gAAB/4AAAf+AAAH/gAAB/4AAAf+AAAH/gAAB/4AAAf+AAAH/gAAB/4
 AAAf+AAAH/wAAD/8AAA//AAAP/4AAH/+AAB//wAA//+AAf//wAP///AP/w=="""
-    icondata = base64.b64decode(iconString)
+    icondata = b64decode(iconString)
     temp_file = "icon.ico"
     icon_file = open(temp_file, "wb")
     icon_file.write(icondata)
@@ -270,20 +268,23 @@ AAAf+AAAH/wAAD/8AAA//AAAP/4AAH/+AAB//wAA//+AAf//wAP///AP/w=="""
     icon = QtGui.QIcon()
     icon.addPixmap(QtGui.QPixmap(temp_file), QtGui.QIcon.Mode.Normal, QtGui.QIcon.State.Off)
     MainWindow.setWindowIcon(icon)
-    os.remove(temp_file)
+    remove(temp_file)
 
     apexID = "1172470"
     release_tag = "v1.1.0"
-    event = threading.Event()
+    event = Event()
     running_text = ["Start Randomizer", "Stop Randomizer"]
     start_stop_log = ["Randomizer stopped!\n", "Randomizer started!"]
     
-    disabled_while_running = [ui.gameDirectoryField,  ui.dpiSelector,        ui.autoDetectButton,
-                              ui.browseButton,        ui.defaultSensSpinbox, ui.minSensSpinbox,
-                              ui.maxSensSpinbox,      ui.saveSettingsButton, ui.enableBindButton,
-                              ui.disableBindButton,   ui.timerCheckbox,      ui.timeSpinbox,
-                              ui.randomizeBindButton, ui.outputLabel,        ui.ctrlCheck,
-                              ui.altCheck,            ui.shiftCheck,         ui.startRandomizerButton]
+    disabled_while_running = [ui.gameDirectoryField,    ui.dpiSelector,         ui.autoDetectButton,
+                              ui.browseButton,          ui.defaultSensSpinbox,  ui.minSensSpinbox,
+                              ui.maxSensSpinbox,        ui.saveSettingsButton,  ui.enableBindButton,
+                              ui.disableBindButton,     ui.timerCheckbox,       ui.timeSpinbox,
+                              ui.randomizeBindButton,   ui.outputLabel,         ui.ctrlCheck,
+                              ui.altCheck,              ui.shiftCheck,          ui.startRandomizerButton,
+                              ui.labelDirectory,        ui.labelDPI,            ui.labelDefault,
+                              ui.labelMin,              ui.labelMax,            ui.labelRandomize,
+                              ui.labelEnable,           ui.labelDisable]
     
     ui.dpiSelector.setCurrentIndex(1)
     config = Config(sens_randomizer_directory)
